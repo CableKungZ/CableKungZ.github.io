f183b2bb8ee0297fcda267b7d52ff2e9ce9514da
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2024-06-01 20:19:16 +0700

    Update daoBuddyService.js

--- source/daoBuddyService.js
+++ source/daoBuddyService.js
@@ -7,7 +7,7 @@ let accounts;
 
 async function connectMetamask() {
     if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
+        web3 = new Web3(window.ethereum); 
         try {
             accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             daoBuddyService = new web3.eth.Contract(contractABI, contractAddress);
