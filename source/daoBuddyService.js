8a4778b4f154e2f7edbe148179262403ce5c0a32
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2024-06-01 20:05:03 +0700

    Update daoBuddyService.js

--- source/daoBuddyService.js
+++ source/daoBuddyService.js
@@ -1,7 +1,9 @@
 const contractAddress = '0x137307cc671DbDaD3C8c50F492Fa921998B9b45D';
 const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"addAllowToken","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"allowToken","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"allowTokenByIndex","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allowedTokens","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"allowedTokensCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approveToken","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"address[]","name":"recipients","type":"address[]"},{"internalType":"uint256[]","name":"amounts","type":"uint256[]"}],"name":"batchTransfer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"},{"internalType":"address[]","name":"recipients","type":"address[]"},{"internalType":"uint256","name":"amountPerRecipient","type":"uint256"}],"name":"batchTransferWithFixAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"consumeContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"fee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"feeAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"feeToken","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"isAllowToken","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isDestroyed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pauseContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"removeAllowToken","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"resumeContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"setFeeAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"setFeeAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"token","type":"address"}],"name":"setFeeToken","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
- 
+const feeTokenAddress = '0xE67E280f5a354B4AcA15fA7f0ccbF667CF74F97b';
+const feeAmount = web3.utils.toWei('0.1', 'ether');
+
 let web3;
 let daoBuddyService;
 let accounts;
@@ -34,29 +36,16 @@ async function loadContract(provider, contractAddress) {
 
 async function ensureAllowance(tokenAddress, requiredAmount) {
     const tokenContract = await loadContract(web3, tokenAddress);
-    await tokenContract.methods.approve(contractAddress, requiredAmount).send({ from: accounts[0] });
-    }
+    const allowance = await tokenContract.methods.allowance(accounts[0], contractAddress).call();
+    const approveAmount = web3.utils.toWei(approveAmount, 'ether');
+    await tokenContract.methods.approve(contractAddress, approveAmount).send({ from: accounts[0] });
 
-
-function parseBatchTransferData(data) {
-    const entries = data.split(',');
-    const addresses = [];
-    const amounts = [];
-
-    for (let i = 0; i < entries.length; i += 2) {
-        addresses.push(entries[i]);
-        amounts.push(web3.utils.toWei(entries[i + 1], 'ether'));
-    }
-
-    return { addresses, amounts };
 }
 
 async function batchTransfer() {
- const feeTokenAddress = '0xE67E280f5a354B4AcA15fA7f0ccbF667CF74F97b';
-const feeAmount = web3.utils.toWei('0.1', 'ether');
     const token = document.getElementById('batchTransferToken').value;
-    const data = document.getElementById('batchTransferData').value;
-    const { addresses, amounts } = parseBatchTransferData(data);
+    const addresses = document.getElementById('batchTransferAddresses').value.split(',');
+    const amounts = document.getElementById('batchTransferAmounts').value.split(',').map(amount => web3.utils.toWei(amount, 'ether'));
 
     try {
         // Ensure allowance for fee token
@@ -74,13 +63,10 @@ const feeAmount = web3.utils.toWei('0.1', 'ether');
 }
 
 async function batchTransferWithFixAmount() {
- const feeTokenAddress = '0xE67E280f5a354B4AcA15fA7f0ccbF667CF74F97b';
-const feeAmount = web3.utils.toWei('0.1', 'ether');
     const token = document.getElementById('batchTransferWithFixAmountToken').value;
     const addresses = document.getElementById('batchTransferWithFixAmountAddresses').value.split(',');
     const amountPerRecipient = web3.utils.toWei(document.getElementById('batchTransferWithFixAmount').value, 'ether');
     const totalAmount = amountPerRecipient * addresses.length;
- 
 
     try {
         // Ensure allowance for fee token
